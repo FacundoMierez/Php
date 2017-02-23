935ae4f2d98b0c296d0ef42f5e378aa10b28594d
FacundoMierez <facundomierez@gmail.com>
2017-02-23 01:13:52 -0300

    estilos

--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -1,17 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+<br>
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">VISTA GENERAL</div>
+        <div class="col-md-8">
+            <a href="" >
+                <img class="img img-responsive img-rounded" src="http://static.minutouno.com/adjuntos/150/imagenes/011/230/0011230322.jpg" alt="Will Neil Young's new tour be worth the wait">
+            </a>
+        </div>
 
-                <div class="panel-body">
-                   Bienvenido 
-                </div>
+        <div class="col-md-4">
+            <div class="search text-center">
+                <h3><span class="glyphicon glyphicon-search"></span> Buscar Articulos</h3>
+            </div>
+            <input type="text" name="search" id="search" class="form-control" title="search" placeholder="Buscar...">
+
+            <div class="search text-center">
+               <h3><span class=""></span>Categorias</h3>
+            </div>
+            <div class="category">
+                <ul>
+                @foreach ($cat as $item)                    
+                    <li role="separator" class="divider"><a href="{{$item->id}}">{{$item->name}}</a></li>
+                @endforeach
+
+                </ul>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
