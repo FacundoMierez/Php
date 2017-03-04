d866d759b9822e02eb6c439e45149582c4394dd4
FacundoMierez <facundomierez@gmail.com>
2017-03-04 15:26:20 -0300

    sesion and login

--- social_auth/app/Http/Controllers/SocialController.php
+++ social_auth/app/Http/Controllers/SocialController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
 
 use Socialite;
 
 class SocialController extends Controller
 {
-   
+    use AuthenticatesUsers;
+
 
     public function redirect($provider)
     {
@@ -17,7 +20,22 @@ class SocialController extends Controller
 
     public function callback($provider){
     	$user = Socialite::driver($provider)->user();
-    	return($user->name);
+    	//$this->login($user->name);
+    	$user_socialite=User::create([
+    		'facebook_id'=>$user->getId(),
+    		'name'=>$user->getName(),
+    		'email'=>$user->getEmail()
+    	]);
 
-    } 
+    	auth()->login($user_socialite);
+    	
+
+    	return redirect('/');
+
+    }
+
+
+
+
+
 }
